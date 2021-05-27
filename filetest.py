2cf57e1c8848f171f6ab96dbc82eeb2f53586a3a
Stefan Merker <stefanmerker@googlemail.com>
2021-05-27 14:24:01 +0200

    filetest mit ordnererstellung und schleife

---
+++ filetest.py
@@ -0,0 +1,17 @@
+import os
+
+ordner = "filetest"
+
+if not os.path.exists(ordner):
+    os.makedirs(ordner)
+
+i = 1
+while i <= 5:
+	j = 1
+	while j <= 5:
+		file_name = "{}\HWN_{}_{}.geojson"
+		file = open(file_name.format(ordner,i,j), "wt")
+		output = "HWN von {} nach {}"
+		file.write(output.format(i,j))
+		j += 1
+	i += 1
--- orstest.py
+++ orstest.py
@@ -1,18 +1,19 @@
 import requests
 
 
-body = {"coordinates":[[8.681495,49.41461],[8.686507,49.41943],[8.687872,49.420318]],"elevation":"true","language":"de-de"}
+body = {"coordinates":[[10.67505,51.75032],[10.66565,51.75445]],"elevation":"true","language":"de-de"}
 
 headers = {
     'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
     'Authorization': '5b3ce3597851110001cf6248902e12deed49439881d71b62e8948798',
     'Content-Type': 'application/json; charset=utf-8'
 }
+
 call = requests.post('https://api.openrouteservice.org/v2/directions/foot-hiking/geojson', json=body, headers=headers)
 
 if call.status_code == 200:
     print("request success")
-    result_file = open("result.geojson", "a")
+    result_file = open("result.geojson", "w")
     result_file.writelines(call.text)
 else:
     output = "request failed ({})"
